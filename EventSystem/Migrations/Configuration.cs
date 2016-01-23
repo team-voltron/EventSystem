09198ad655c34ac206ab9ae565a5cc67dedc1f31
georgimanov <georgimanov@gmail.com>
2016-01-23 13:50:34 +0200

    fixed connection string

--- EventSystem/Migrations/Configuration.cs
+++ EventSystem/Migrations/Configuration.cs
@@ -8,7 +8,7 @@ namespace EventSystem.Migrations
     {
         public Configuration()
         {
-            this.AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationsEnabled = true;
             this.AutomaticMigrationDataLossAllowed = true;
         }
 
