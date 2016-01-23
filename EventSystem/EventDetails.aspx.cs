9bfca11818649b6469ca2cf69fcf218e8d11d776
georgimanov <georgimanov@gmail.com>
2016-01-23 20:10:06 +0200

    Event details page added

---
+++ EventSystem/EventDetails.aspx.cs
@@ -0,0 +1,34 @@
+﻿namespace EventSystem
+{
+    using System;
+    using System.Linq;
+    using System.Web.ModelBinding;
+
+    using Models;
+
+    public partial class EventDetails : System.Web.UI.Page
+    {
+        private EventSystemDbContext dbContext;
+
+        public EventDetails()
+        {
+            this.dbContext = new EventSystemDbContext();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public Event FormVideEventDetails_GetItem([QueryString("id")]int? eventId)
+        {
+            if (eventId == null)
+            {
+                this.Response.Redirect("~/");
+            }
+
+            var currentEvent = this.dbContext.Events.FirstOrDefault(e => e.Id == eventId);
+
+            return currentEvent;
+        }
+    }
+}
