c8b9d98fa29b32f1083d9a19eb07de81d210eebd
georgimanov <georgimanov@gmail.com>
2016-01-23 10:36:44 +0200

    initial commit

---
+++ EventSystem/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EventSystem.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EventSystem/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventSystem.Startup))]
+namespace EventSystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
