9a32fb0596f97e38c69b55717837395a52d385bd
georgimanov <georgimanov@gmail.com>
2016-01-24 17:34:47 +0200

    edit categories in admin part

--- EventSystem/Admin/EditCategories.aspx.cs
+++ EventSystem/Admin/EditCategories.aspx.cs
@@ -1,17 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace EventSystem.Admin
+﻿namespace EventSystem.Admin
 {
+    using System;
+    using System.Linq;
+    using System.Web.UI.HtmlControls;
+
+    using Models;
+
     public partial class EditCategories : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        private EventSystemDbContext dbContext;
+        private HtmlGenericControl insertRow;
 
+        public EditCategories()
+        {
+            this.dbContext = new EventSystemDbContext();
+        }
+
+        public IQueryable<Category> ListViewCategories_GetData()
+        {
+            return this.dbContext.Categories.OrderBy(cat => cat.Name);
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void ListViewCategories_UpdateItem(int ID)
+        {
+            var category = this.dbContext.Categories.Find(ID);
+            if (category == null)
+            {
+                this.ModelState.AddModelError(string.Empty, string.Format("Item with id {0} was not found", ID));
+
+                return;
+            }
+
+            this.TryUpdateModel(category);
+            if (this.ModelState.IsValid)
+            {
+                this.dbContext.SaveChanges();
+            }
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void ListViewCategories_DeleteItem(int ID)
+        {
+            var category = this.dbContext.Categories.Find(ID);
+            this.dbContext.Categories.Remove(category);
+            this.dbContext.SaveChanges();
+        }
+
+        public void ListViewCategories_InsertItem()
+        {
+            var newCategory = new Category();
+            this.TryUpdateModel(newCategory);
+            if (this.ModelState.IsValid)
+            {
+                this.dbContext.Categories.Add(newCategory);
+                this.dbContext.SaveChanges();
+            }
         }
     }
 }
